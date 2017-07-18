09232765dcf0631d2405c6292187359285ee5890
root <wern17@gmail.com>
2017-07-18 19:14:01 +0700

    init errorUtils

---
+++ errorUtils.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/appengine/log"
+	"net/http"
+	"runtime/debug"
+)
+
+func handlePanic(ctx context.Context, w http.ResponseWriter) {
+	if r := recover(); r != nil {
+		if err, ok := r.(error); ok {
+			log.Errorf(ctx, err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		} else if errStr, ok := r.(string); ok {
+			log.Errorf(ctx, errStr)
+			http.Error(w, errStr, http.StatusInternalServerError)
+		}
+		log.Errorf(ctx, "%s", debug.Stack())
+	}
+}
+
+func PanicIfError(err error, args ...interface{}) {
+	if err != nil {
+		additionalErrorMsg := ""
+		for _, arg := range args {
+			additionalErrorMsg += arg.(string) + "|"
+		}
+		panic(additionalErrorMsg + err.Error())
+	}
+}
